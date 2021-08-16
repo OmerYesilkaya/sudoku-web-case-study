208b36dfc4b4c1b20a0940a728ae7512b16c316b
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-16 22:18:05 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { Center } from "@chakra-ui/react";
+import GameHistory from "pages/GameHistory";
+import GameSelect from "pages/GameSelect";
+import Home from "pages/Home";
+import Sudoku from "pages/Sudoku";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
-	return <div>test</div>;
+	return (
+		<Center w="100vw" h="100vh">
+			<Router>
+				<Switch>
+					<Route path="/sudoku" exact>
+						<GameSelect />
+					</Route>
+					<Route path="/sudoku/:id" exact>
+						<Sudoku />
+					</Route>
+					<Route path="/history">
+						<GameHistory />
+					</Route>
+
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</Router>
+		</Center>
+	);
 }
 
 export default App;
