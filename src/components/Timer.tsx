62a001b339cd511253b146f2ad5b769f29711d2d
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-21 22:59:46 +0300

    create timer component that tracks and shows time of the game

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,28 +1,20 @@
 import { Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useSudokuStore } from "store/useSudokuStore";
+import toReadableTime from "utils/toReadableTime";
 
 export default function Timer() {
-	const [time, setTime] = useState<number>(0);
-
-	function toReadableTime(time: number) {
-		const seconds = time % 60;
-		time = Math.floor(time / 60);
-		const minutes = time % 60;
-		time = Math.floor(time / 60);
-		const hours = time % 24;
-
-		return `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-	}
+	const { timePassed, incrementTime } = useSudokuStore((state) => ({ timePassed: state.timePassed, incrementTime: state.incrementTime }));
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setTime((prev) => prev + 1);
+			incrementTime();
 		}, 1000);
 
 		return () => {
 			clearInterval(interval);
 		};
-	}, []);
+	}, [incrementTime]);
 
-	return <Text fontSize="xl">{toReadableTime(time)}</Text>;
+	return <Text fontSize="xl">{toReadableTime(timePassed)}</Text>;
 }
