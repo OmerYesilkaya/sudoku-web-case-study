d5eba729eec4d71e08b482e06814db92d1b18c02
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-21 23:01:42 +0300

    clean up and add functionality to sudoku buttons

--- src/pages/Sudoku.tsx
+++ src/pages/Sudoku.tsx
@@ -1,20 +1,47 @@
-import { Button, Center, Flex } from "@chakra-ui/react";
+import { Box, Button, Center, Flex } from "@chakra-ui/react";
 import Board from "components/Board";
+import Numpad from "components/Numpad";
 import Timer from "components/Timer";
+import { useEffect } from "react";
+import { MdChevronLeft } from "react-icons/md";
+import { useHistory } from "react-router-dom";
+import { useSudokuStore } from "store/useSudokuStore";
 
 export default function Sudoku() {
+	const history = useHistory();
+	const { undo, redo } = useSudokuStore((state) => ({ undo: state.undo, redo: state.redo }));
+
+	useEffect(() => {
+		function handleKeyDown(e: KeyboardEvent) {
+			if (e.ctrlKey && e.key === "z") {
+				undo();
+			} else if (e.ctrlKey && e.key === "y") {
+				redo();
+			}
+		}
+
+		window.addEventListener("keydown", handleKeyDown);
+
+		return () => window.removeEventListener("keydown", handleKeyDown);
+	}, [redo, undo]);
+
 	return (
-		<Center w="100%" h="100%">
-			<Flex w="75vh" h="75vh" direction="column">
-				<Board />
-				<Flex justify="space-between" align="center" mt="1em">
-					<Timer />
-					<Flex>
-						<Button colorScheme="green">Check Correctness</Button>
-						<Button colorScheme="orange" ml="0.5em">
-							Undo
-						</Button>
-					</Flex>
+		<Center w="100%" h="100%" position="relative">
+			<Button colorScheme="orange" position="absolute" top="10px" right="10px" onClick={() => history.push("/sudoku")}>
+				<MdChevronLeft size="1.5em" />
+				Go back to Sudoku selection
+			</Button>
+			<Flex>
+				<Box position="relative">
+					<Center position="absolute" top="-30px" left="5px">
+						<Timer />
+					</Center>
+
+					<Numpad />
+				</Box>
+
+				<Flex w="75vh" h="75vh" direction="column">
+					<Board />
 				</Flex>
 			</Flex>
 		</Center>
