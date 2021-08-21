ad134f7c1afe3997b28ebcafc3cf4f1468489454
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-21 23:02:36 +0300

    create game history page

--- src/pages/GameHistory.tsx
+++ src/pages/GameHistory.tsx
@@ -1,3 +1,35 @@
+import { Button, Center, Flex, Text } from "@chakra-ui/react";
+import Header from "components/common/Header";
+import { useSudokuStore } from "store/useSudokuStore";
+import { RiArrowGoBackLine } from "react-icons/ri";
+import { useHistory } from "react-router-dom";
+
 export default function GameHistory() {
-	return null;
+	const { gameHistory } = useSudokuStore((state) => ({ gameHistory: state.gameHistory }));
+	const history = useHistory();
+	return (
+		<Center w="100vw" h="100vh">
+			<Flex direction="column" w="50%" maxH="80vh" h="80vh" overflow="auto">
+				<Header>Game History</Header>
+				<Flex direction="column" w="100%">
+					{gameHistory.length > 0 ? (
+						gameHistory.map((game) => (
+							<Flex borderRadius="md" color="gray.900" mb="0.3em" bg="gray.300" py="1em" px="1.5em" key={game.id} w="100%">
+								Sudoku {game.id}
+								<Text ml="auto">{game.time}</Text>
+							</Flex>
+						))
+					) : (
+						<Text border="2px dashed white" p="1em">
+							There are no finished games yet. Please finish a game first.
+						</Text>
+					)}
+				</Flex>
+
+				<Button colorScheme="orange" mt="auto" w="max-content" mx="auto" onClick={() => history.push("/")}>
+					Go back <RiArrowGoBackLine size="1.3em" />
+				</Button>
+			</Flex>
+		</Center>
+	);
 }
